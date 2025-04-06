b34158e6e303349080fccd202ab1ec2fca3f297b
Hansh <hans.havoc@gmail.com>
2025-04-05 22:03:58 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,41 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+
+    private String titulo;
+    private String descriçao;
+    private int cargaHoraria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescriçao() {
+        return descriçao;
+    }
+
+    public void setDescriçao(String descriçao) {
+        this.descriçao = descriçao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descriçao='" + descriçao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
